db2602df32266955adc3823e6b66420d5370f186
applenob <nobking@126.com>
2017-01-15 21:24:33 +0800

    add gru_csv

--- iii_rnn_namer.py
+++ iii_rnn_namer.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 """
 @author: cer
-加载模型，无则训练
+应用入口
+调用模型接口，实现起名
 """
 
 from rnn_theano import *
@@ -14,7 +15,7 @@ def generate_name(model, name_len, first_name, X_train, y_train, char_to_index, 
     new_name = [char_to_index[first_name]]
     for i in range(name_len-1):
         next_word_probs = model.forward_propagation(new_name)
-        # print "next_word_probs: ",  next_word_probs
+        print "next_word_probs: ",  next_word_probs
         # print sum(sum(next_word_probs))
         samples = np.random.multinomial(1, next_word_probs[-1])
         sampled_word = np.argmax(samples)
@@ -23,7 +24,7 @@ def generate_name(model, name_len, first_name, X_train, y_train, char_to_index, 
     return name_str
 
 
-if __name__ == '__main__':
+def namer_rnn_one_hot():
     # np.random.seed(1)
     # X_train, y_train, char_to_index, index_to_char = load_training_data()
     X_train, y_train, char_to_index, index_to_char = load_sample_training_data(0)
@@ -42,7 +43,7 @@ if __name__ == '__main__':
         model = RNNTheano(char_num)
         # losses = train_with_sgd(model, X_train, y_train, nepoch=50)
         # save_model_parameters_theano('./data/trained-model-theano.npz', model)
-        load_model_parameters_theano('model/model-80-3296-2017-01-14-21-30-39.npz', model)
+        load_model_parameters_theano('model/rnn_one_hot/model-80-3296-2017-01-14-21-30-39.npz', model)
         ignore_num = 0
         i = 0
         while i <= name_num:
@@ -56,3 +57,6 @@ if __name__ == '__main__':
             i += 1
         print "ignored samples: ", ignore_num
 
+
+if __name__ == '__main__':
+    namer_rnn_one_hot()
---
+++ v_gru_c2v.py
@@ -0,0 +1,268 @@
+import numpy as np
+import theano as theano
+import theano.tensor as T
+from theano.gradient import grad_clip
+import time
+import operator
+import sys
+import os
+from datetime import datetime
+from ii_data_process import load_training_data, load_sample_training_data
+
+
+LEARNING_RATE = 0.001
+EMBEDDING_DIM = 48
+HIDDEN_DIM = 128
+NEPOCH = 20
+MODEL_OUTPUT_FILE = "model/gru_c2v/model-80-5264-2017-01-14-16-47-15.npz"
+PRINT_EVERY = 25000
+
+
+class GRUTheano:
+    def __init__(self, word_dim, hidden_dim=128, bptt_truncate=-1):
+        # Assign instance variables
+        self.word_dim = word_dim
+        self.hidden_dim = hidden_dim
+        self.bptt_truncate = bptt_truncate
+        # Initialize the network parameters
+        E = np.random.uniform(-np.sqrt(1. / word_dim), np.sqrt(1. / word_dim), (hidden_dim, word_dim))
+        U = np.random.uniform(-np.sqrt(1. / hidden_dim), np.sqrt(1. / hidden_dim), (6, hidden_dim, hidden_dim))
+        W = np.random.uniform(-np.sqrt(1. / hidden_dim), np.sqrt(1. / hidden_dim), (6, hidden_dim, hidden_dim))
+        V = np.random.uniform(-np.sqrt(1. / hidden_dim), np.sqrt(1. / hidden_dim), (word_dim, hidden_dim))
+        b = np.zeros((6, hidden_dim))
+        c = np.zeros(word_dim)
+        # Theano: Created shared variables
+        self.E = theano.shared(name='E', value=E.astype(theano.config.floatX))
+        self.U = theano.shared(name='U', value=U.astype(theano.config.floatX))
+        self.W = theano.shared(name='W', value=W.astype(theano.config.floatX))
+        self.V = theano.shared(name='V', value=V.astype(theano.config.floatX))
+        self.b = theano.shared(name='b', value=b.astype(theano.config.floatX))
+        self.c = theano.shared(name='c', value=c.astype(theano.config.floatX))
+        # SGD / rmsprop: Initialize parameters
+        self.mE = theano.shared(name='mE', value=np.zeros(E.shape).astype(theano.config.floatX))
+        self.mU = theano.shared(name='mU', value=np.zeros(U.shape).astype(theano.config.floatX))
+        self.mV = theano.shared(name='mV', value=np.zeros(V.shape).astype(theano.config.floatX))
+        self.mW = theano.shared(name='mW', value=np.zeros(W.shape).astype(theano.config.floatX))
+        self.mb = theano.shared(name='mb', value=np.zeros(b.shape).astype(theano.config.floatX))
+        self.mc = theano.shared(name='mc', value=np.zeros(c.shape).astype(theano.config.floatX))
+        # We store the Theano graph here
+        self.theano = {}
+        self.__theano_build__()
+
+    def __theano_build__(self):
+        E, V, U, W, b, c = self.E, self.V, self.U, self.W, self.b, self.c
+
+        x = T.ivector('x')
+        y = T.ivector('y')
+
+        def forward_prop_step(x_t, s_t1_prev, s_t2_prev):
+            # This is how we calculated the hidden state in a simple RNN. No longer!
+            # s_t = T.tanh(U[:,x_t] + W.dot(s_t1_prev))
+
+            # Word embedding layer
+            x_e = E[:, x_t]
+
+            # GRU Layer 1
+            z_t1 = T.nnet.hard_sigmoid(U[0].dot(x_e) + W[0].dot(s_t1_prev) + b[0])
+            r_t1 = T.nnet.hard_sigmoid(U[1].dot(x_e) + W[1].dot(s_t1_prev) + b[1])
+            c_t1 = T.tanh(U[2].dot(x_e) + W[2].dot(s_t1_prev * r_t1) + b[2])
+            s_t1 = (T.ones_like(z_t1) - z_t1) * c_t1 + z_t1 * s_t1_prev
+
+            # GRU Layer 2
+            z_t2 = T.nnet.hard_sigmoid(U[3].dot(s_t1) + W[3].dot(s_t2_prev) + b[3])
+            r_t2 = T.nnet.hard_sigmoid(U[4].dot(s_t1) + W[4].dot(s_t2_prev) + b[4])
+            c_t2 = T.tanh(U[5].dot(s_t1) + W[5].dot(s_t2_prev * r_t2) + b[5])
+            s_t2 = (T.ones_like(z_t2) - z_t2) * c_t2 + z_t2 * s_t2_prev
+
+            # Final output calculation
+            # Theano's softmax returns a matrix with one row, we only need the row
+            o_t = T.nnet.softmax(V.dot(s_t2) + c)[0]
+
+            return [o_t, s_t1, s_t2]
+
+        [o, s, s2], updates = theano.scan(
+            forward_prop_step,
+            sequences=x,
+            truncate_gradient=self.bptt_truncate,
+            outputs_info=[None,
+                          dict(initial=T.zeros(self.hidden_dim)),
+                          dict(initial=T.zeros(self.hidden_dim))])
+
+        prediction = T.argmax(o, axis=1)
+        o_error = T.sum(T.nnet.categorical_crossentropy(o, y))
+
+        # Total cost (could add regularization here)
+        cost = o_error
+
+        # Gradients
+        dE = T.grad(cost, E)
+        dU = T.grad(cost, U)
+        dW = T.grad(cost, W)
+        db = T.grad(cost, b)
+        dV = T.grad(cost, V)
+        dc = T.grad(cost, c)
+
+        # Assign functions
+        self.predict = theano.function([x], o)
+        self.predict_class = theano.function([x], prediction)
+        self.ce_error = theano.function([x, y], cost)
+        self.bptt = theano.function([x, y], [dE, dU, dW, db, dV, dc])
+
+        # SGD parameters
+        learning_rate = T.scalar('learning_rate')
+        decay = T.scalar('decay')
+
+        # rmsprop cache updates
+        mE = decay * self.mE + (1 - decay) * dE ** 2
+        mU = decay * self.mU + (1 - decay) * dU ** 2
+        mW = decay * self.mW + (1 - decay) * dW ** 2
+        mV = decay * self.mV + (1 - decay) * dV ** 2
+        mb = decay * self.mb + (1 - decay) * db ** 2
+        mc = decay * self.mc + (1 - decay) * dc ** 2
+
+        self.sgd_step = theano.function(
+            [x, y, learning_rate, theano.Param(decay, default=0.9)],
+            [],
+            updates=[(E, E - learning_rate * dE / T.sqrt(mE + 1e-6)),
+                     (U, U - learning_rate * dU / T.sqrt(mU + 1e-6)),
+                     (W, W - learning_rate * dW / T.sqrt(mW + 1e-6)),
+                     (V, V - learning_rate * dV / T.sqrt(mV + 1e-6)),
+                     (b, b - learning_rate * db / T.sqrt(mb + 1e-6)),
+                     (c, c - learning_rate * dc / T.sqrt(mc + 1e-6)),
+                     (self.mE, mE),
+                     (self.mU, mU),
+                     (self.mW, mW),
+                     (self.mV, mV),
+                     (self.mb, mb),
+                     (self.mc, mc)
+                     ])
+
+    def calculate_total_loss(self, X, Y):
+        return np.sum([self.ce_error(x, y) for x, y in zip(X, Y)])
+
+    def calculate_loss(self, X, Y):
+        # Divide calculate_loss by the number of words
+        num_words = np.sum([len(y) for y in Y])
+        return self.calculate_total_loss(X, Y) / float(num_words)
+
+
+def train_with_sgd(model, X_train, y_train, learning_rate=0.001, nepoch=20, decay=0.9,
+    callback_every=10000, callback=None):
+    num_examples_seen = 0
+    for epoch in range(nepoch):
+        # For each training example...
+        for i in np.random.permutation(len(y_train)):
+            # One SGD step
+            model.sgd_step(X_train[i], y_train[i], learning_rate, decay)
+            num_examples_seen += 1
+            # Optionally do callback
+            if (callback and callback_every and num_examples_seen % callback_every == 0):
+                callback(model, num_examples_seen)
+    return model
+
+
+def save_model_parameters_theano(model, outfile):
+    np.savez(outfile,
+        E=model.E.get_value(),
+        U=model.U.get_value(),
+        W=model.W.get_value(),
+        V=model.V.get_value(),
+        b=model.b.get_value(),
+        c=model.c.get_value())
+    print "Saved model parameters to %s." % outfile
+
+
+def load_model_parameters_theano(path, modelClass=GRUTheano):
+    npzfile = np.load(path)
+    E, U, W, V, b, c = npzfile["E"], npzfile["U"], npzfile["W"], npzfile["V"], npzfile["b"], npzfile["c"]
+    hidden_dim, word_dim = E.shape[0], E.shape[1]
+    print "Building model model from %s with hidden_dim=%d word_dim=%d" % (path, hidden_dim, word_dim)
+    sys.stdout.flush()
+    model = modelClass(word_dim, hidden_dim=hidden_dim)
+    model.E.set_value(E)
+    model.U.set_value(U)
+    model.W.set_value(W)
+    model.V.set_value(V)
+    model.b.set_value(b)
+    model.c.set_value(c)
+    return model
+
+
+def gradient_check_theano(model, x, y, h=0.001, error_threshold=0.01):
+    # Overwrite the bptt attribute. We need to backpropagate all the way to get the correct gradient
+    model.bptt_truncate = 1000
+    # Calculate the gradients using backprop
+    bptt_gradients = model.bptt(x, y)
+    # List of all parameters we want to chec.
+    model_parameters = ['E', 'U', 'W', 'b', 'V', 'c']
+    # Gradient check for each parameter
+    for pidx, pname in enumerate(model_parameters):
+        # Get the actual parameter value from the mode, e.g. model.W
+        parameter_T = operator.attrgetter(pname)(model)
+        parameter = parameter_T.get_value()
+        print "Performing gradient check for parameter %s with size %d." % (pname, np.prod(parameter.shape))
+        # Iterate over each element of the parameter matrix, e.g. (0,0), (0,1), ...
+        it = np.nditer(parameter, flags=['multi_index'], op_flags=['readwrite'])
+        while not it.finished:
+            ix = it.multi_index
+            # Save the original value so we can reset it later
+            original_value = parameter[ix]
+            # Estimate the gradient using (f(x+h) - f(x-h))/(2*h)
+            parameter[ix] = original_value + h
+            parameter_T.set_value(parameter)
+            gradplus = model.calculate_total_loss([x],[y])
+            parameter[ix] = original_value - h
+            parameter_T.set_value(parameter)
+            gradminus = model.calculate_total_loss([x],[y])
+            estimated_gradient = (gradplus - gradminus)/(2*h)
+            parameter[ix] = original_value
+            parameter_T.set_value(parameter)
+            # The gradient for this parameter calculated using backpropagation
+            backprop_gradient = bptt_gradients[pidx][ix]
+            # calculate The relative error: (|x - y|/(|x| + |y|))
+            relative_error = np.abs(backprop_gradient - estimated_gradient)/(np.abs(backprop_gradient) + np.abs(estimated_gradient))
+            # If the error is to large fail the gradient check
+            if relative_error > error_threshold:
+                print "Gradient Check ERROR: parameter=%s ix=%s" % (pname, ix)
+                print "+h Loss: %f" % gradplus
+                print "-h Loss: %f" % gradminus
+                print "Estimated_gradient: %f" % estimated_gradient
+                print "Backpropagation gradient: %f" % backprop_gradient
+                print "Relative Error: %f" % relative_error
+                return
+            it.iternext()
+        print "Gradient check for parameter %s passed." % (pname)
+
+
+if __name__ == '__main__':
+    # Load data
+    X_train, y_train, char_to_index, index_to_char = load_sample_training_data(1)
+    VOCABULARY_SIZE = len(char_to_index.keys())
+    if not MODEL_OUTPUT_FILE:
+        ts = datetime.now().strftime("%Y-%m-%d-%H-%M")
+        MODEL_OUTPUT_FILE = "GRU-%s-%s-%s-%s.dat" % (ts, VOCABULARY_SIZE, EMBEDDING_DIM, HIDDEN_DIM)
+
+
+    # Build model
+    model = GRUTheano(VOCABULARY_SIZE, hidden_dim=HIDDEN_DIM, bptt_truncate=-1)
+
+    # Print SGD step time
+    t1 = time.time()
+    model.sgd_step(X_train[10], y_train[10], LEARNING_RATE)
+    t2 = time.time()
+    print "SGD Step time: %f milliseconds" % ((t2 - t1) * 1000.)
+    sys.stdout.flush()
+
+    # We do this every few examples to understand what's going on
+    def sgd_callback(model, num_examples_seen):
+        dt = datetime.now().isoformat()
+        loss = model.calculate_loss(X_train[:10000], y_train[:10000])
+        print("\n%s (%d)" % (dt, num_examples_seen))
+        print("--------------------------------------------------")
+        print("Loss: %f" % loss)
+        save_model_parameters_theano(model, MODEL_OUTPUT_FILE)
+        print("\n")
+        sys.stdout.flush()
+    for epoch in range(NEPOCH):
+        train_with_sgd(model, X_train, y_train, learning_rate=LEARNING_RATE, nepoch=1, decay=0.9,
+                       callback_every=PRINT_EVERY, callback=sgd_callback)
